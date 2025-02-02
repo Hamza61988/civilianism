4f46c72f8a998745af9f00c613007f352de5841d
Hamza <deadshot6198@gmail.com>
2025-02-02 18:05:18 +0500

    wwwwwwwwwww

--- src/Images.js
+++ src/Images.js
@@ -4,12 +4,11 @@ import './Images.css'; // or './Images.module.css' if using CSS modules
 export function Images() {
  
   const imageSources = [
-    "/images/work.png",  
-    "/images/work2.png",
-    "/images/work3.png",
-    "/images/imagee.png",
-    "imagee2.png",
-    
+    "https://media.discordapp.net/attachments/1294402226248220703/1324209547639914507/1592.png?ex=67a0303e&is=679edebe&hm=fc74bc9a627874dbd9472bea98c7ba7fe14a914a70441e178e7375d9b19148d1&=&format=webp&quality=lossless&width=437&height=437",
+    "https://media.discordapp.net/attachments/1294402226248220703/1307765139268833340/3651.png?ex=67a05972&is=679f07f2&hm=b29f4278053ae252bb446a0f0e3c16a4d0b91bf5c6cdf67b9a328325bb4ca7d8&=&format=webp&quality=lossless&width=636&height=636",
+   "https://media.discordapp.net/attachments/1294402226248220703/1305074551918694410/0015.png?ex=67a072e4&is=679f2164&hm=8e27c16ceb5c293b3013f005657c0d9621554fcc21fab2efb493a06dc83b35c8&=&format=webp&quality=lossless&width=636&height=636",
+   ,"https://media.discordapp.net/attachments/1294402226248220703/1305074551918694410/0015.png?ex=67a072e4&is=679f2164&hm=8e27c16ceb5c293b3013f005657c0d9621554fcc21fab2efb493a06dc83b35c8&=&format=webp&quality=lossless&width=636&height=636",
+   "https://media.discordapp.net/attachments/1294402226248220703/1330024498950307880/2146.png?ex=67a03fd7&is=679eee57&hm=d3636163a2d1eb0a761e2d7a0542382e7fe7099bbe1e363a74866db4b36ef806&=&format=webp&quality=lossless&width=636&height=636"
   ];
   
 
