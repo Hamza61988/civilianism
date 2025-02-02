ecd4d608d9074ce9800780f4bc4c8af3f7f18a4b
Hamza <deadshot6198@gmail.com>
2025-02-02 17:27:08 +0500

    Updated package files and Main.js

--- src/Main.js
+++ src/Main.js
@@ -5,7 +5,7 @@ const Main = () => {
   return (
     <div id="civilianism"  className="wrapper">
       <div  className="container">
-        <img className="centered-image" src='/image.png' alt='wassup' />
+        <img className="centered-image" src='https://civilianismclub.com/static/media/The-Seven-Church-Apostles.54a8fc798b1c38a0d691.webp' alt='wassup' />
       </div>
       <h5 className="title">Civilianism Club</h5>
     </div>
